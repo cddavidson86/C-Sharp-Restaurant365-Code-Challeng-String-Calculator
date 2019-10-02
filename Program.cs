cd7c53277a3fe3ab53e777981eabefbd0c046a19
Charles Davidson <cddavidson86@yahoo.com>
2019-10-02 14:18:10 -0500

    Challenge 2 completed. Unlimited numbers can be added.

--- Program.cs
+++ Program.cs
@@ -6,18 +6,43 @@ namespace R365
     {
         static void Main(string[] args)
         {
+            // Enter input and store it as a string called inputString.
             Console.WriteLine("Please enter your input below: ");
-            var mainString = Console.ReadLine();
+            String inputString = Console.ReadLine();
+          
+            // The current separator for the delimiter. 
+            String[] separator = { "," }; 
+
+            // Splitting the string into an array called strList.
+            String[] strList = inputString.Split(separator, StringSplitOptions.RemoveEmptyEntries); 
+
+            // Int var that holds the total addition.
+            int totalCount = 0; 
+    
+                // Parsing the string into and int and adding. Else throw exception. 
+                foreach(String value in strList) 
+                { 
+                    int number;
+            
+                    bool success = Int32.TryParse(value, out number);
+
+                    if (success)
+                    {
+                        totalCount += number;         
+                    }
+                    else
+                    {
+                        Console.WriteLine("Attempted conversion of '{0}' failed. '{0}' is not a number.", 
+                                        value ?? "<null>");
+                    }
+                } 
+                // Display total
+                Console.WriteLine(totalCount); 
         }
 
-        // static string checkerFunction(string name)
+        // static string checkerFunction(string strlist)
         // {
-        //     if (name == "") 
-        //     {
-        //         Console.WriteLine("We got nothing, please type your name.");
-        //         return Console.ReadLine();
-        //     }
-        //     return name;
+          
         // }
     }
 }
